9f1aa2776a73d93d8979ea4c1d0dbdae1e5596c5
Melendez Guajardo <e.melendez.guajardo@accenture.com>
2016-06-29 16:35:09 -0500

    second commit

---
+++ index.js
@@ -0,0 +1,84 @@
+var request = require('request'),
+cheerio = require('cheerio'),
+_ = require("underscore"),
+http = require('http'),
+fs = require('fs');
+//download = require('download-file');
+// const readline = require('readline');
+
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+
+var websitelink;
+var allAbsoluteLinks = [];
+var pageToVisit = "http://downloads.khinsider.com/game-soundtracks/album/the-legend-of-zelda-majora-s-mask-original-soundtrack";
+var uniqueLinks = [];
+// console.log("Visiting page " + pageToVisit);
+
+// rl.question('websitelink: ', (answer) => {
+//     websitelink = answer;
+//     rl.close();
+    
+    
+// });
+
+request(pageToVisit, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+
+        $ = cheerio.load(body);
+        var table = $('table');
+        
+        var absoluteLinks = $("a[href^='http']");
+        
+
+        var filtered = $(absoluteLinks).filter(function(){
+            return pageToVisit == $(this).attr('href').substring(0,pageToVisit.length);
+        });
+
+        filtered.each(function() {
+            allAbsoluteLinks.push($(this).attr('href'));
+                // console.log($(this).attr('href'));
+        });
+
+        uniqueLinks = _.uniq(allAbsoluteLinks,true);
+        // console.log(uniqueLinks);
+        console.log("Found " + uniqueLinks.length + " absolute links");
+        // var options = {
+        //     directory: "C:/test"
+        // }
+
+        for (var i = 0; i < uniqueLinks.length; i++) {
+            request(uniqueLinks[i],function (error, response, body){
+                $ = cheerio.load(body);
+                var audioLink = $('audio');
+                var filename = audioLink.attr('src').split("/")[6];
+                console.log("downloading... "+filename);
+                http.get(audioLink.attr('src'),function(otherResponse){
+                    var file = fs.createWriteStream("C:/test/"+filename);
+                    otherResponse.on('data', function(chunk){
+                        file.write(chunk);
+                    }).on('end', function(){
+                        console.log("finished "+file.path);
+                        file.end();
+                    });
+                });
+                // console.log("downloading..."+uniqueLinks[i]);
+                // download(audioLink.attr('src'), options, function(err){
+                //     if (err) throw err
+                //     console.log("boom")
+                // });
+            });
+        };
+        
+
+        
+
+        // for (var i = uniqueLinks.length - 1; i >= 0; i--) {
+        
+        // };
+    } else {
+     console.log("Error: " + error);
+   }
+});
